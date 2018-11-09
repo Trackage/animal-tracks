82654e16e224412c81a447604ab9b35da368b357
Michael Sumner <mdsumner@gmail.com>
2018-11-09 00:59:51 +0000

    something I was doing ...

--- examples/read_as_trip_segments.R
+++ examples/read_as_trip_segments.R
@@ -15,3 +15,23 @@ trip::explode(tr)
 library(silicate)
 SC(tr) ## silicate edge type with all data maintained
 
+
+
+sc_coord.tbl_df <- function(x, ...) {
+  if (dplyr::is_grouped_df(x)) {
+   ## remove grouping columns 
+    gvars <- attr(x, "vars")
+    x <- ungroup(x)
+    x[gvars] <- NULL
+  } 
+  x
+}
+
+sc_path.tbl_df <- function(x, ...) {
+  if (dplyr::is_grouped_df(x)) {
+    ## remove grouping columns 
+    gvars <- attr(x, "vars")
+    x <- ungroup(x)
+    x[gvars] <- NULL
+  }
+}
