86363d4fa41659ce0e46d183c72c8f13b34290b9
Michael Sumner <mdsumner@gmail.com>
2018-11-09 00:24:04 +0000

    not entirely successful

---
+++ gganimating/elephantseal_ice/elephantseal_ice.R
@@ -0,0 +1,25 @@
+library(gganimate)
+library(sf)
+library(dplyr)
+ice <- readRDS("ice/ellie-ice-contour15pc.rds")
+ele <- readRDS("ellie_IMOS.RDS")
+lon_0 <- round(mean(ele$lon), 1)
+lat_0 <- round(mean(ele$lat), 1)
+proj <- glue::glue("+proj=stere +lon_0={lon_0} +lat_0={lat_0} +datum=WGS84")
+ele[c("X", "Y")] <- rgdal::project(as.matrix(ele[c("lon", "lat")]), proj)
+slc <- ele %>% dplyr::filter(between(date, min(ice$date), max(ice$date))) %>% distinct(id)
+sub <- ele %>% dplyr::inner_join(slc %>% sample_frac(.2), "id") %>% sample_frac(.01) %>% 
+  #rename(ele_date = date) %>% 
+  rename(ele = id) %>% 
+  arrange(ele, date)
+
+pice <- st_transform(ice, proj) %>% dplyr::rename(ice_date = date)
+ggplot() + 
+  geom_sf(data = pice %>% dplyr::filter((row_number() %% 10) == 1)) + 
+  transition_manual(ice_date) +
+  geom_path(data = sub , aes(X, Y, group = ele, colour = ele)) +
+ 
+ transition_reveal(ele, date)
+ 
+
+
